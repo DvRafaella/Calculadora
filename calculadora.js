631960bbcad7a87880221463f4002363e975f3bd
Gawarez <gabrieltbaggio@gmail.com>
2025-07-28 17:07:29 -0300

    agora sim tratando os 2 inputs

--- calculadora.js
+++ calculadora.js
@@ -11,7 +11,7 @@ function calcular(){
     const operacao = seletor.value;
 
     if(operacao==="somar")
-       resposta.innerHTML = valor1 + valor2;
+        resposta.innerHTML = valor1 + valor2;
 
     if(operacao==="subtrair")
         resposta.innerHTML = valor1 - valor2;
@@ -20,7 +20,7 @@ function calcular(){
         resposta.innerHTML = valor1 * valor2;
 
     if(operacao==="dividir") {
-        if (campo2.value === '0') {
+        if (campo1.value === '0' || campo2.value === '0') {
             resposta.innerHTML = 'Divisão por 0!';
         } else {
             resposta.innerHTML = valor1 / valor2;
